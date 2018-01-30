486d02a6ba6c919e3e99619b38857bbd5b127a3c
bazyl <svintcov95@gmail.com>
2018-01-30 23:12:22 +0200

    entity added

---
+++ src/main/java/entity/Department.java
@@ -0,0 +1,20 @@
+package entity;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+  private int id;
+  private String name;
+  private List<Employee> employees;
+}
---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,21 @@
+package entity;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+  private int id;
+  private String name;
+  private List<Department> departments;
+
+}
