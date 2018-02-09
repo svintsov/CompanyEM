c63ea7194be99205c0c3e781b67b89760aea672f
bazyl <svintcov95@gmail.com>
2018-02-09 21:39:35 +0200

    added ContextListener

--- src/main/java/servlet/ContextListener.java
+++ src/main/java/servlet/ContextListener.java
@@ -1,5 +1,25 @@
 package servlet;
 
-public class ContextListener {
+import action.Action;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
+public class ContextListener implements ServletContextListener{
+
+  Map<String, Action> actions;
+
+  @Override
+  public void contextInitialized(ServletContextEvent servletContextEvent) {
+    actions = new ConcurrentHashMap<>();
+
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent servletContextEvent) {
+    actions = null;
+  }
 }
