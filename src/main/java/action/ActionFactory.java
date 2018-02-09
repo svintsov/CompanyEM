3a38214721cf298a3059d0f6a7c6eb5486832ad7
bazyl <svintcov95@gmail.com>
2018-02-10 01:20:43 +0200

    log4j2 fixing

--- src/main/java/action/ActionFactory.java
+++ src/main/java/action/ActionFactory.java
@@ -3,13 +3,17 @@ package action;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ActionFactory {
+  //private static final Logger logger = LogManager.getLogger();
 
   public static Action getAction(HttpServletRequest request) {
     Map<String, Action> actions = (ConcurrentHashMap<String, Action>) request.getServletContext()
         .getAttribute("actions");
     String path = request.getRequestURI().replace("/CompanyEm/page","");
+    //logger.info("Accessing page:"+path);
     return actions.get(path);
   }
 }
--- src/main/java/servlet/Servlet.java
+++ src/main/java/servlet/Servlet.java
@@ -8,14 +8,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @WebServlet("/page/*")
 public class Servlet extends HttpServlet {
+
+ // private static final Logger logger = LogManager.getLogger();
+
 
   public void doGet(HttpServletRequest request,
       HttpServletResponse response) throws ServletException, IOException {
     service(request, response);
   }
+
 
   public void doPost(HttpServletRequest request,
       HttpServletResponse response) throws ServletException, IOException {
@@ -24,9 +30,12 @@ public class Servlet extends HttpServlet {
 
   protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+   // logger.info("Servlet work");
     try {
       Action action = ActionFactory.getAction(request);
       String view = action.execute(request, response);
+
+     // logger.info("Accessed page:"+view);
 
       request.getRequestDispatcher("/jsp/" + view + ".jsp").forward(request, response);
 
