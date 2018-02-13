1c47b036e80439ac81187d78e347ae6edfee19fa
bazyl <svintcov95@gmail.com>
2018-02-13 23:07:01 +0200

    abstract dao class added

---
+++ src/main/java/dao/CrudDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudDao<T, ID> {
+
+  T save(T entity) throws SQLException;
+
+  T update(T entity) throws SQLException;
+
+  T findOne(ID id) throws SQLException;
+
+  List<T> findAll() throws SQLException;
+
+  void delete(ID id) throws SQLException;
+
+  List<T> findAll(long limit, long offset) throws SQLException;
+}
---
+++ src/main/java/util/constant/general/Global.java
@@ -0,0 +1,8 @@
+package util.constant.general;
+
+public interface Global {
+  String USER_ROLE = "USER_ROLE";
+  String ADMIN_ROLE = "ADMIN_ROLE";
+  String ID = "id";
+  String NAME = "name";
+}
